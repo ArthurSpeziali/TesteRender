14fc30ae6015317fc8883b96994b91e3e6f2ec59
Arthur Speziali <arthur.speziali2022@gmail.com>
2025-06-09 20:05:29 -0300

    Resolving prod deploy steps

--- config/runtime.exs
+++ config/runtime.exs
@@ -33,7 +33,7 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "example.com"
+  host = System.get_env("RENDER_EXTERNAL_HOSTNAME") || "localhost"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :teste_render, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
@@ -48,7 +48,8 @@ if config_env() == :prod do
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
       port: port
     ],
-    secret_key_base: secret_key_base
+  secret_key_base: secret_key_base,
+  server: true
 
   # ## SSL Support
   #
